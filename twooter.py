efa6336d6cbe022b4531b1435682cca7d405fc59
wbdana <william.b.dana@gmail.com>
2018-01-03 19:28:19 -0500

    Build basic bot

---
+++ twooter.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import tweepy, time, sys
+from .secret import CONSUMER_KEY, CONSUMER_SECRET, ACCESS_KEY, ACCESS_SECRET
+
+argfile = str(sys.argv[1])
+
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+api = tweepy.API(auth)
+
+filename = open(argfile, 'r')
+f = filename.readlines()
+filename.close()
+
+for line in f:
+    api.update_status(line)
+    time.sleep(900)
